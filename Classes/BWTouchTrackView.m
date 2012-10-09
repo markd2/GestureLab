3949b73d2b6a00a5bb1aab905e0648748a78b9b2
Mark Dalrymple <markd@borkware.com>
2012-10-09 13:51:47 -0400

    added stub touch methods

--- Classes/BWTouchTrackView.m
+++ Classes/BWTouchTrackView.m
@@ -1,10 +1,46 @@
 #import "BWTouchTrackView.h"
 
+typedef enum : NSInteger {
+    kStateReadyToTrack,
+    kStateTracking
+} TrackingState;
+
 static const CGFloat kPromptTextSize = 36.0;
+
+
+@interface BWTouchTrackView () {
+    TrackingState _state;
+}
+
+@end // extension
 
 
 @implementation BWTouchTrackView
 
+- (void) commonInit {
+    self.userInteractionEnabled = YES;
+    self.multipleTouchEnabled = YES;
+} // commonInit
+
+
+- (id) initWithFrame: (CGRect) frame {
+    if ((self = [super initWithFrame: frame])) {
+        [self commonInit];
+    }
+
+    return self;
+
+} // initWithFrame
+
+
+- (id) initWithCoder: (NSCoder *) decoder {
+    if ((self = [super initWithCoder: decoder])) {
+        [self commonInit];
+    }
+
+    return self;
+
+} // initWithCoder
 
 
 - (void) drawBackground: (CGRect) rect {
@@ -50,6 +86,29 @@ static const CGFloat kPromptTextSize = 36.0;
     [self drawFrame: bounds];
 
 } // drawRect
+
+// --------------------------------------------------
+
+- (void) touchesBegan: (NSSet *) touches  withEvent: (UIEvent *) event {
+    NSLog (@"BEGAN");
+} // touchesBegan
+
+
+- (void) touchesMoved: (NSSet *) touches  withEvent: (UIEvent *) event {
+    NSLog (@"MOVED");
+
+} // touchesMoved
+
+
+- (void) touchesEnded: (NSSet *) touches  withEvent: (UIEvent *) event {
+    NSLog (@"ENDED");
+} // touchesEnded
+
+
+- (void) touchesCancelled: (NSSet *) touches  withEvent: (UIEvent *) event {
+    NSLog (@"Cancelled");
+
+} // touchesCancelled
 
 @end // BWTouchView
 
